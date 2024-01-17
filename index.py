7f29c74ad0ad8aeded0c9cb8f6871e9b5b6353ba
Md Mustak Talukder <mustak@Mds-MacBook-Air.local>
2024-01-17 23:45:44 +0100

    git up for share

---
+++ index.py
@@ -0,0 +1,51 @@
+import cv2
+from deepface import DeepFace
+
+face_cascade_data = cv2.CascadeClassifier('/Users/mustak/Desktop/uni/RT/project/haarcascade_frontalface_default.xml')
+print(face_cascade_data)
+
+def captureVideo():
+    return cv2.VideoCapture(0)
+
+def faceDetaction(frame):
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    return face_cascade_data.detectMultiScale(gray,1.1,4)
+
+def emotionDetaction(frame):
+    result = DeepFace.analyze(img_path = frame , actions=['emotion'], enforce_detection=False )
+    print(type(result))
+    print(result)
+    print(result[0]['dominant_emotion'])
+    emotion = result[0]['dominant_emotion']
+    return str(emotion)
+
+def showResultInlive(frame,txt):
+    cv2.putText(frame,txt,(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3)
+    cv2.imshow('frame',frame)
+
+
+
+captureVideoFrame = captureVideo()
+
+
+while True:
+    ret,frame = captureVideoFrame.read()
+
+    allFaces = faceDetaction(frame)
+
+    print(allFaces)
+    for (x,y,w,h) in allFaces:
+        # print(x,y,w,h)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3)
+
+    emotionName = emotionDetaction(frame)
+    # txt = "Face"
+
+    showResultInlive(frame,emotionName)
+
+    # cnt+q for quit
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+captureVideoFrame.release()
+cv2.destroyAllWindows()
